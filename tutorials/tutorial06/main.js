2d242886d74aead16e14eacd5a7c1b02cffe4616
calstyles <cstyles2@unca.edu>
2023-02-15 15:02:29 -0500

    Edit 1

--- tutorials/tutorial06/main.js
+++ tutorials/tutorial06/main.js
@@ -15,9 +15,26 @@
  *    3. If you choose Twitter, allow your user to specify both a search term
  *       and a result_type (mixed, recent, or popular).
  */
-const search = ev =>{
+async function search(){
     ev.preventDefault();
 
     const searchTerm = document.querySelector('#search_term').value;
     const location = document.querySelector('#location').value;
-}
+    const url = `https://www.apitutor.org/yelp/simple/v3/businesses/search?location=${location}&term=${searchTerm}`;
+    const response = await fetch(url);
+    const data = await response.json();
+  
+    const results = data.businesses.map((business) => {
+      const categories = business.categories.map((category) => category.title).join(', ');
+      return `
+        <div class="result">
+          <h2><a href="${business.url}" target="_blank">${business.name}</a></h2>
+          <p>${categories}</p>
+          <p>${business.location.address1}, ${business.location.city}, ${business.location.state} ${business.location.zip_code}</p>
+        </div>
+      `;
+    }).join('');
+  
+    const resultsDiv = document.getElementById('results');
+    resultsDiv.innerHTML = results;
+  }
