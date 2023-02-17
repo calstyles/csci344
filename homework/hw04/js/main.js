0ef6f67451c97f4a0c7c8fc999698e8683ee859d
calstyles <cstyles2@unca.edu>
2023-02-17 15:16:39 -0500

    Function Added

--- homework/hw04/js/main.js
+++ homework/hw04/js/main.js
@@ -1,6 +1,26 @@
 import {getAccessToken} from './utilities.js';
 const rootURL = 'https://photo-app-secured.herokuapp.com';
 
+const showUserProfile = async (token) => {
+    const endpoint = `${rootURL}/api/profile`;
+    const response = await fetch(endpoint, {
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + token
+        }
+    });
+    const data = await response.json();
+    console.log('User Profile:', data);
+
+    const userPic = document.querySelector('.user-pic');
+    const usernameRec = document.querySelector('.username-rec');
+    const usernameTop = document.querySelector('.username-top');
+
+    userPic.src = data.profile_pic;
+    usernameRec.textContent = data.username;
+    usernameTop.textContent = data.username;
+};
+
 const showStories = async (token) => {
     const endpoint = `${rootURL}/api/stories`;
     const response = await fetch(endpoint, {
@@ -17,7 +37,6 @@ const showPosts = async (token) => {
     console.log('code to show posts');
 }
 
-
 const initPage = async () => {
     // first log in (we will build on this after Spring Break):
     const token = await getAccessToken(rootURL, 'webdev', 'password');
@@ -25,6 +44,7 @@ const initPage = async () => {
     // then use the access token provided to access data on the user's behalf
     showStories(token);
     showPosts(token);
+    showUserProfile(token);
 }
 
 initPage();
