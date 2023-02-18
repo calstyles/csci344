a6c10c45580426a9dcf5065da86c9746c2611d1f
Caleb Styles <cstyles2@unca.edu>
2023-02-18 17:33:42 -0500

    showUserProfile updated

--- homework/hw04/js/main.js
+++ homework/hw04/js/main.js
@@ -1,20 +1,16 @@
 import {getAccessToken} from './utilities.js';
 const rootURL = 'https://photo-app-secured.herokuapp.com';
 
-const showUserProfile = async (token) => {0
+const showUserProfile = async (token) => {
     const endpoint = `${rootURL}/api/profile`;
-    const headers = new Headers({'Content-Type': 'application/json'}, {'Authorization' : `Bearer ${token}`});
-    const response = await fetch(endpoint, headers);
+    const response = await fetch(endpoint, {
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + token
+        }
+    })
     const data = await response.json();
     console.log('User Profile:', data);
-
-    const userPic = document.querySelector('.user-pic');
-    const usernameRec = document.querySelector('.username-rec');
-    const usernameProfile = document.querySelector('.username-profile');
-
-    userPic.src = data.profile_pic;
-    usernameRec.textContent = data.username;
-    usernameProfile.textContent = data.username;
 };
 
 const showStories = async (token) => {
