f3640871ee6a20f847c22d32cf2dde237b234aa8
Caleb Styles <cstyles2@unca.edu>
2023-03-14 00:52:55 -0400

    Added Unlike Function

--- tutorials/tutorial08/src/components/LikeButton.js
+++ tutorials/tutorial08/src/components/LikeButton.js
@@ -22,10 +22,35 @@ export default function LikeButton({post, requeryPost, token}) {
         requeryPost();
     }
 
-    return (
-        <div className="heart">
-            <a className="icon-properties"><i className="far fa-heart fa-regular" onClick={likePost}></i></a>
-        </div>
-    ); 
+    async function unLikePost(){
+        const endpoint = "https://photo-app-secured.herokuapp.com/api/posts/likes/";
+        const response = await fetch(endpoint, {
+            method: "DELETE",
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + token
+            }
+        })
+    
+        const data = await response.json();
+        requeryPost();
+    }
+
+
+    
+    if(post.current_user_like_id != null){
+        console.log(post.current_user_like_id);
+        return (
+            <div className="heart">
+                <a className="icon-properties"><i className="far fa-heart fa-regular" onClick={likePost}></i></a>
+            </div>
+        ); 
+    }else{
+        return (
+            <div className="heart">
+                <a className="icon-properties"><i className="fas fa-heart liked_post" onClick={unLikePost}></i></a>
+            </div>
+        ); 
+    }
 
 }
