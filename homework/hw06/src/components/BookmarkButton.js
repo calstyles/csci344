f627ec73bced493814f339324372cbe140e92089
calstyles <cstyles2@unca.edu>
2023-03-16 11:51:24 -0400

    Bookmark Added

---
+++ homework/hw06/src/components/BookmarkButton.js
@@ -0,0 +1,52 @@
+import React from 'react';
+
+export default function BookmarkButton({post, currentBookmarkId, requeryPost, token}) { 
+    async function bookmarkPost(){
+        const endpoint = "https://photo-app-secured.herokuapp.com/api/bookmarks";
+        const postData = {
+            "post_id": post.id
+        };
+
+        const response = await fetch(endpoint, {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + token
+            },
+            body: JSON.stringify(postData)
+        })
+        const data = await response.json();
+        requeryPost();
+    }
+
+    async function unBookmarkPost(){
+        const endpoint = "https://photo-app-secured.herokuapp.com/api/bookmarks/" + currentBookmarkId;
+        const response = await fetch(endpoint, {
+            method: "DELETE",
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + token
+            }
+        })
+
+        const data = await response.json();
+        requeryPost();
+    }
+        //     return `<div class="bookmark" id="bookmark_${currentPost.id}"><button class="icon-properties" onclick="deleteBookmark(${currentPost.id}, ${currentPost.current_user_bookmark_id}, ${i}, ${length})" aria-checked="true" aria-label="bookmark ${i + 1} of ${length}"><i class="fas fa-bookmark"></i></button></div>`;
+        // }
+        // return `<div class="bookmark" id="bookmark_${currentPost.id}"><button class="icon-properties" onclick="createBookmark(${currentPost.id}, ${i}, ${length})" aria-checked="false" aria-label="bookmark ${i + 1} of ${length}"><i class="far fa-bookmark"></i></button></div>`;
+
+    if (currentBookmarkId != null) {
+        console.log("like has been called");
+        return(
+            <div className="bookmark">
+                <a className="icon-properties"><i class="fas fa-bookmark" onClick={unBookmarkPost}></i></a>
+            </div>
+        );
+    }
+    return(
+        <div class="bookmark">
+            <a class="icon-properties"><i class="far fa-bookmark" onClick={bookmarkPost}></i></a>
+        </div>
+    );
+}
--- homework/hw06/src/components/Post.js
+++ homework/hw06/src/components/Post.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import LikeButton from './LikeButton';
+import BookmarkButton from './BookmarkButton';
 import {getHeaders} from '../utils';
 
 export default function Post({post, token}) { 
     const [currentLikeId, setCurrentLikeId] = useState(post.current_user_like_id);
+    const [currentBookmarkId, setCurrentBookmarkId] = useState(post.current_user_bookmark_id);
 
     async function requeryPost(){
         const response = await fetch("https://photo-app-secured.herokuapp.com/api/posts/" + post.id, {
@@ -15,14 +17,16 @@ export default function Post({post, token}) {
         });
         const data = await response.json();
         setCurrentLikeId(data.current_user_like_id);
+        setCurrentBookmarkId(data.current_user_bookmark_id);
     }
-    
+
     return (
         <div className="card">   
             <div key={"post_username_" + post.id}>{post.user.username}</div>
             <img src={post.image_url} key={"post_image_" + post.id}></img>
             <div key={"post_caption_" + post.id}>{post.caption}</div>
             <LikeButton post={post} currentLikeId={currentLikeId} requeryPost={requeryPost} token={token}/>
+            <BookmarkButton post={post} currentBookmarkId ={currentBookmarkId} requeryPost={requeryPost} token={token}/>
         </div>
     );     
 }
