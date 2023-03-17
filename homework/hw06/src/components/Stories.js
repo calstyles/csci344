b6e5553694abe0d02efe3853fe9989c794ded01e
calstyles <cstyles2@unca.edu>
2023-03-17 14:39:33 -0400

    Stories completed

--- homework/hw06/src/components/Stories.js
+++ homework/hw06/src/components/Stories.js
@@ -1,9 +1,35 @@
 import React from 'react';
+import {getHeaders} from '../utils';
+import { useState, useEffect } from "react";
 
-export default function Stories(token) { 
-   
+export default function Stories({token}) { 
+    const[stories, setStories] = useState(null);
+
+    useEffect(() => {
+        async function fetchStories(){
+                const response = await fetch('https://photo-app-secured.herokuapp.com/api/stories', {
+                    headers: getHeaders(token)
+                });
+                const data = await response.json();
+                setStories(data);
+        }
+        fetchStories();
+    }, [token]);
+
+    console.log(stories);
+
+    if (stories==null) {
+        return '';
+    }
     return (
-        <div>Your Stories here...</div>
+        stories.map(story=>{
+            console.log(story);
+            return (
+                    <div className='story-panel'>
+                        <div><img src={story.user.image_url} className="story-image"></img></div>
+                        <a className="story-username">{story.user.username}</a>
+                    </div>
+            )
+        })
     ); 
-
 }
