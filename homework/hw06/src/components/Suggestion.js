9939f5081fe8a7aa5e994687c00edae8e340efb7
Caleb Styles <cstyles2@unca.edu>
2023-03-18 02:51:56 -0400

    Suggestion Progress has been made

--- homework/hw06/src/components/Suggestion.js
+++ homework/hw06/src/components/Suggestion.js
@@ -3,89 +3,77 @@ import {getHeaders} from '../utils';
 import { useState, useEffect } from "react";
 
     
-export default function Suggestion({suggestion, token}) { 
-    const [isFollowing, setIsFollowing] = useState(null);
-
-
-    async function requerySuggestion(){
-        let isFollowing = false;
-        const response = await fetch('/api/following', {
-            headers: getHeaders(token)
+export default function Suggestion({ suggestion, token }) {
+    const [isFollowing, setIsFollowing] = useState(false);
+    const [currentFollow, setCurrentFollow] = useState(null);
+  
+    async function requerySuggestion() {
+      const response = await fetch('/api/following', {
+        headers: getHeaders(token),
+      });
+      const data = await response.json();
+      const isFollowing = data.some((user) => user.id === suggestion.id);
+      setIsFollowing(isFollowing);
+    }
+  
+    useEffect(() => {
+      requerySuggestion();
+    }, []);
+  
+    async function followUser() {
+      const endpoint = 'https://photo-app-secured.herokuapp.com/api/following';
+      const postData = {
+        user_id: suggestion.id,
+      };
+      const response = await fetch(endpoint, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: 'Bearer ' + token,
+        },
+        body: JSON.stringify(postData),
+      });
+      const data = await response.json();
+      console.log(data);
+      
+      setCurrentFollow(data.following.id);
+      setIsFollowing(true);
+    }
+  
+    async function unfollowUser() {
+        console.log(currentFollow);
+        const endpoint = 'https://photo-app-secured.herokuapp.com/api/following/' + currentFollow;
+        const response = await fetch(endpoint, {
+            method: 'DELETE',
+            headers: {
+            'Content-Type': 'application/json',
+            Authorization: 'Bearer ' + token,
+            },
         });
         const data = await response.json();
-        // console.log("follow are here");
-        // console.log(data);
-        
-        for(let i = 0; i < data.length; i++){
-            if(suggestion == data[i]){
-                isFollowing = true;
-            }
-        }
-        setIsFollowing(isFollowing);
-    }  
-
-    async function followUser(){
-        const endpoint = "https://photo-app-secured.herokuapp.com/api/following";
-        const postData = {
-            "user_id": suggestion.id
-        };
-
-        const response = await fetch(endpoint, {
-            method: "POST",
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': 'Bearer ' + token
-            },
-            body: JSON.stringify(postData)
-        })
-        const data = await response.json();
-        requerySuggestion();
+        console.log(data);
+        setIsFollowing(false);
+        setCurrentFollow(null);
     }
-
-    async function unfollowUser(){
-        const endpoint = "https://photo-app-secured.herokuapp.com/api/following/" + suggestion.id;
-        const response = await fetch(endpoint, {
-            method: "DELETE",
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': 'Bearer ' + token
-            }
-        })
-
-        const data = await response.json();
-        requerySuggestion();
-    }
-
-    console.log(isFollowing);
-
-    if(!isFollowing){
-        return (
-            <div className="suggestion">
-                <img className="user-pic" src={suggestion.image_url} key={"suggestion_image_" + suggestion.id}></img>
-                <div className='suggestion-words'>
-                    <div className="suggestion-info">
-                        <div key={"suggestion_username_" + suggestion.id}>{suggestion.username}</div>
-                        <div className="suggested-for-you">Suggested for you</div>
+  
+    return (
+        <div className="suggestion">
+            <img className="user-pic" src={suggestion.image_url} key={'suggestion_image_' + suggestion.id}/>
+            <div className="suggestion-words">
+                <div className="suggestion-info">
+                    <div key={'suggestion_username_' + suggestion.id}>
+                        {suggestion.username}
                     </div>
-                    <div className="followButton">
+                    <div className="suggested-for-you">Suggested for you</div>
+                </div>
+                <div className="followButton">
+                    {isFollowing && suggestion.id == currentFollow? (
+                        <button type="submit" className="icon-properties" onClick={unfollowUser}>unfollow</button>
+                        ) : (
                         <button type="submit" className="icon-properties" onClick={followUser}>follow</button>
-                    </div>
+                    )}
                 </div>
             </div>
-        ); 
-    }
-
-    requerySuggestion();
-    return (
-        <div className="suggested-accounts">
-            <img className="user-pic" src={suggestion.image_url} key={"suggestion_image_" + suggestion.id}></img>
-            <div className="suggestion-info">
-                <div key={"suggestion_username_" + suggestion.id}>{suggestion.username}</div>
-                <div className="suggested-for-you">Suggested for you</div>
-                <div className="followButton">
-                    <button type="submit" className="icon-properties" onClick={unfollowUser}>unfollow</button>
-                </div>
-            </div>    
-        </div>
-    );        
-}
+      </div>
+    );
+  }
