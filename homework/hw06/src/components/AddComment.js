68e0bbe834f074746c8efe20369b187cec1ee8d4
Caleb Styles <cstyles2@unca.edu>
2023-03-19 02:45:02 -0400

    Started AddComments, WIP

---
+++ homework/hw06/src/components/AddComment.js
@@ -0,0 +1,7 @@
+export default function AddComment({post, currentComments, requeryPost, token}) {
+    return(
+        <div>
+            <h1>Add a Comment Here</h1>
+        </div>
+    );
+}
--- homework/hw06/src/components/Post.js
+++ homework/hw06/src/components/Post.js
@@ -1,21 +1,29 @@
 import React, { useState, useEffect } from 'react';
 import LikeButton from './LikeButton';
 import BookmarkButton from './BookmarkButton';
+import AddComment from './AddComment';
 import {getHeaders} from '../utils';
 
 export default function Post({post, token}) { 
     const [currentLikeId, setCurrentLikeId] = useState(post.current_user_like_id);
     const [currentBookmarkId, setCurrentBookmarkId] = useState(post.current_user_bookmark_id);
     const [currentLikeLength, setCurrentLikeLength] = useState(post.likes.length);
+    const [firstComment, setFirstComment] = useState(post.comments[0]);
+    // const [recentComment, setRecentComment] = useState(post.comments[post.comments.length-1].id);
 
     async function requeryPost(){
         const response = await fetch("https://photo-app-secured.herokuapp.com/api/posts/" + post.id, {
                     headers: getHeaders(token)
         });
         const data = await response.json();
+        console.log(data);
+        console.log(data.comments[0]);
+        console.log(data.comments);
         setCurrentLikeId(data.current_user_like_id);
         setCurrentBookmarkId(data.current_user_bookmark_id);
-        setCurrentLikeLength(data.likes.length);        
+        setCurrentLikeLength(data.likes.length);  
+        setFirstComment(data.comments[0]);      
+        // setRecentComment(data.comments[data.comments.length - 1].id);
     }
 
     return (
@@ -26,6 +34,7 @@ export default function Post({post, token}) {
             <LikeButton post={post} currentLikeId={currentLikeId} requeryPost={requeryPost} token={token} key={"likeButton_" + post.id}/>
             <div>{currentLikeLength} likes</div>
             <BookmarkButton post={post} currentBookmarkId ={currentBookmarkId} requeryPost={requeryPost} token={token} key={"bookmarkButton_" + post.id}/>
+            <AddComment post={post} firstComment = {firstComment} requeryPost={requeryPost} token={token} key={"post_" + post.id} />
         </div>
     );     
 }
