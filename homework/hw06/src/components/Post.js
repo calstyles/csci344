687c0c894e08d9a0bb1023bfbf2648213e4e8440
Caleb Styles <cstyles2@unca.edu>
2023-03-27 22:31:50 -0400

    Added Close Button

--- homework/hw06/src/components/Post.js
+++ homework/hw06/src/components/Post.js
@@ -5,45 +5,66 @@ import AddComment from './AddComment';
 import { getHeaders } from '../utils';
 
 export default function Post({ post, token, index, length }) {
-  const [currentLikeId, setCurrentLikeId] = useState(post.current_user_like_id);
-  const [currentBookmarkId, setCurrentBookmarkId] = useState(post.current_user_bookmark_id);
-  const [currentLikeLength, setCurrentLikeLength] = useState(post.likes.length);
-  const [showAllComments, setShowAllComments] = useState(false);
-  const [comments, setComments] = useState(post.comments);
+    const [currentLikeId, setCurrentLikeId] = useState(post.current_user_like_id);
+    const [currentBookmarkId, setCurrentBookmarkId] = useState(post.current_user_bookmark_id);
+    const [currentLikeLength, setCurrentLikeLength] = useState(post.likes.length);
+    const [showAllComments, setShowAllComments] = useState(false);
+    const [comments, setComments] = useState(post.comments);
 
-  async function requeryPost() {
-    const response = await fetch(
-      'https://photo-app-secured.herokuapp.com/api/posts/' + post.id,
-      {
-        headers: getHeaders(token),
-      }
-    );
-    const data = await response.json();
-    setCurrentLikeId(data.current_user_like_id);
-    setCurrentBookmarkId(data.current_user_bookmark_id);
-    setCurrentLikeLength(data.likes.length);
-    setComments(data.comments);
-  }
+    async function requeryPost() {
+        const response = await fetch('https://photo-app-secured.herokuapp.com/api/posts/' + post.id, {
+            headers: getHeaders(token),
+          }
+        );
+        const data = await response.json();
+        setCurrentLikeId(data.current_user_like_id);
+        setCurrentBookmarkId(data.current_user_bookmark_id);
+        setCurrentLikeLength(data.likes.length);
+        setComments(data.comments);
+    }
 
-  const likeLabel = `like ${index + 1} of ${length}`;
-  const bookmarkLabel = `bookmark ${index + 1} of ${length}`;
+    const likeLabel = `like ${index + 1} of ${length}`;
+    const bookmarkLabel = `bookmark ${index + 1} of ${length}`;
 
-  return (
-    <div className="card">
-      <div key={'post_username_' + post.id}>{post.user.username}</div>
-      <img src={post.image_url} key={'post_image_' + post.id}></img>
-      <div key={'post_caption_' + post.id}>{post.caption}</div>
-      <LikeButton post={post} currentLikeId={currentLikeId} requeryPost={requeryPost} token={token} ariaLabel={likeLabel} key={'like_' + post.id}/>
-      <div>{currentLikeLength} likes</div>
-      <BookmarkButton post={post} currentBookmarkId={currentBookmarkId} requeryPost={requeryPost} setCurrentBookmarkId={setCurrentBookmarkId} token={token} ariaLabel={bookmarkLabel} key={'bookmarkButton_' + post.id}/>
-      {comments.slice(0, showAllComments ? comments.length : 1).map((comment) => (
-        <div key={'comment_' + comment.id}>
-          <div>{comment.user.username}</div>
-          <div>{comment.text}</div>
+    const handleViewCommentsClick = () => {
+        setShowAllComments(true);
+    };
+
+    const handleCloseModalClick = () => {
+        setShowAllComments(false);
+    };
+
+    return (
+        <div className="card">
+            <div key={'post_username_' + post.id}>{post.user.username}</div>
+            <img src={post.image_url} key={'post_image_' + post.id}></img>
+            <div key={'post_caption_' + post.id}>{post.caption}</div>
+            <LikeButton post={post} currentLikeId={currentLikeId} requeryPost={requeryPost} token={token} ariaLabel={likeLabel} key={'like_' + post.id}/>
+            <div>{currentLikeLength} likes</div>
+            <BookmarkButton post={post} currentBookmarkId={currentBookmarkId} requeryPost={requeryPost} setCurrentBookmarkId={setCurrentBookmarkId} token={token} ariaLabel={bookmarkLabel} key={'bookmarkButton_' + post.id}/>
+            {comments.slice(0, showAllComments ? comments.length : 1).map((comment) => (
+                <div key={'comment_' + comment.id}>
+                    <div>{comment.user.username}</div>
+                    <div>{comment.text}</div>
+                </div>
+            ))}
+            {comments.length > 1 && !showAllComments && (
+              <button onClick={handleViewCommentsClick}>View all {comments.length} comments</button>
+            )}
+            {showAllComments && (
+                <div className="modal">
+                    <div className="modal-content">
+                        {comments.map((comment) => (
+                            <div key={'comment_' + comment.id}>
+                                <div>{comment.user.username}</div>
+                                <div>{comment.text}</div>
+                            </div>
+                        ))}
+                        <button onClick={handleCloseModalClick}>Close</button>
+                    </div>
+                </div>
+            )}
+            <AddComment post={post} requeryPost={requeryPost} token={token} key={"post_" + post.id} />
         </div>
-      ))}
-      {comments.length > 1 && !showAllComments && (<button onClick={() => setShowAllComments(true)}>View all {comments.length} comments</button>)}
-      <AddComment post={post} requeryPost={requeryPost} token={token} key={"post_" + post.id} />
-    </div>
-  );
+    );
 }
