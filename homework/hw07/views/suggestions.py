4b93dc96bc82548b7a23b01d73b3b911617b165b
Caleb Styles <cstyles2@unca.edu>
2023-03-31 23:43:50 -0400

    Suggestions Finished

--- homework/hw07/views/suggestions.py
+++ homework/hw07/views/suggestions.py
@@ -10,9 +10,12 @@ class SuggestionsListEndpoint(Resource):
         self.current_user = current_user
     
     def get(self):
-        # suggestions should be any user with an ID that's not in this list:
-        # print(get_authorized_user_ids(self.current_user))
-        return Response(json.dumps([]), mimetype="application/json", status=200)
+        authorized_user_ids = get_authorized_user_ids(self.current_user)
+        all_user_ids = [user.id for user in User.query.all()]
+        suggested_user_ids = [id for id in all_user_ids if id not in authorized_user_ids]
+        suggested_user_ids = suggested_user_ids[:7]
+        suggested_users = [user.to_dict() for user in User.query.filter(User.id.in_(suggested_user_ids))]
+        return Response(json.dumps(suggested_users), mimetype="application/json", status=200)
 
 
 def initialize_routes(api):
