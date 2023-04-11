811f894add0b13d1062605e254be2d545cf3d2d7
Caleb Styles <cstyles2@unca.edu>
2023-04-10 22:43:03 -0400

    Comment POST WIP, Comment DELETE Finished

--- homework/hw07/views/comments.py
+++ homework/hw07/views/comments.py
@@ -9,21 +9,39 @@ class CommentListEndpoint(Resource):
         self.current_user = current_user
     
     def post(self):
-        # create a new "Comment" based on the data posted in the body 
         body = request.get_json()
-        print(body)
-        return Response(json.dumps({}), mimetype="application/json", status=201)
-        
+        if body is None:
+            return Response(json.dumps({'error': 'Request body is empty or content-type header is not set to "application/json".'}), status=400)
+        # create a new "Comment" based on the data posted in the body 
+        post = Post.query.get(body['post_id'])
+        if post is None:
+            return Response(json.dumps({'error': f'Post with id {body["post_id"]} not found.'}), status=404)
+
+        comment = Comment(text=body['text'], user_id=self.current_user.id, post_id=post.id)
+
+        db.session.add(comment)
+        db.session.commit()
+
+        return Response(json.dumps(comment.to_dict()), mimetype="application/json", status=201)
+
 class CommentDetailEndpoint(Resource):
 
     def __init__(self, current_user):
         self.current_user = current_user
   
     def delete(self, id):
-        # delete "Comment" record where "id"=id
-        print(id)
-        return Response(json.dumps({}), mimetype="application/json", status=200)
+        comment = Comment.query.filter_by(id=id).first()
 
+        if comment is None:
+            return Response(json.dumps({"error": "Comment not found"}), mimetype="application/json", status=404)
+
+        if comment.user_id != self.current_user.id:
+            return Response(json.dumps({"error": "You are not authorized to delete this comment"}), mimetype="application/json", status=404)
+
+        db.session.delete(comment)
+        db.session.commit()
+
+        return Response(json.dumps({}), mimetype="application/json", status=200)
 
 def initialize_routes(api):
     api.add_resource(
