5480d031b92663092ca8d41abc0ddd2cb76f54fb
Caleb Styles <cstyles2@unca.edu>
2023-04-14 01:56:08 -0400

    Comments WIP

--- homework/hw07/views/post_likes.py
+++ homework/hw07/views/post_likes.py
@@ -11,31 +11,34 @@ class PostLikesListEndpoint(Resource):
     def post(self):
         # create a new "like_post" based on the data posted in the body 
         body = request.get_json()
-        post_id = body.get('post_id')
 
         if body is None:
             return Response(json.dumps({'error': 'Request body is empty or content-type header is not set to "application/json".'}), status=400)
         
-        if isinstance(body['post_id'], str):
+        post_id = body.get('post_id')
+
+        if not isinstance(post_id, int):
             return Response(json.dumps({'error': 'Invalid post_id format. Must be an integer.'}), status=400)
         
         if not post_id:
-            return Response(json.dumps({'error': 'post_id is required'}), mimetype="application/json", status=404)
+            return Response(json.dumps({'error': 'post_id is required'}), mimetype="application/json", status=400)
 
-        like = LikePost.query.get(body['post_id'])
+        like = LikePost.query.filter_by(user_id=self.current_user.id, post_id=post_id).first()
 
-        if like is None:
-            return Response(json.dumps({'error': f'Post with id {body["post_id"]} not found.'}), status=404)
+        if like is not None:
+            return Response(json.dumps({'error': 'You have already liked this post.'}), status=400)
         
-        if like.user_id != self.current_user.id:
-            return Response(json.dumps({'error': f'You are not authorized to add a like on this post.'}), status=404)
+        post = LikePost.query.get(post_id)
 
+        if post is None:
+            return Response(json.dumps({'error': f'Post with id {post_id} not found.'}), status=404)
+        
         like_post = LikePost(user_id=self.current_user.id, post_id=post_id)
         db.session.add(like_post)
         db.session.commit()
 
         return Response(json.dumps(like_post.to_dict()), mimetype="application/json", status=201)
-    
+
 class PostLikesDetailEndpoint(Resource):
 
     def __init__(self, current_user):
