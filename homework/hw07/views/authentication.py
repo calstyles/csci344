ea042863dec1f80c4327f07c2d137b9577d6e5ea
Caleb Styles <cstyles2@unca.edu>
2023-04-26 03:42:24 -0400

    So close

--- homework/hw07/views/authentication.py
+++ homework/hw07/views/authentication.py
@@ -1,6 +1,7 @@
 from flask import request, \
     make_response, render_template, redirect
 from models import User
+import datetime
 import flask_jwt_extended
 
 def logout():
@@ -14,9 +15,35 @@ def logout():
 
 def login():
     if request.method == 'POST':
-        print('See lecture 25 video + starter files')
-        return 'See lecture 25 video + starter files'
-    else:
+        print(request.form)
+        username = request.form.get('username')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(username=username).one_or_none()
+
+        if user is None:
+            return render_template(
+                'login.html',
+                message = 'username not in database'
+            )        
+        
+        if user and not user.check_password(password):
+            return render_template(
+                'login.html',
+                message='bad password'
+            )
+
+        expires = datetime.timedelta(minutes=20)
+        access_token = flask_jwt_extended.create_access_token(
+            identity=user.id,
+            expires_delta=expires
+        )
+
+        response = make_response(redirect('/', 302))
+        flask_jwt_extended.set_access_cookies(response, access_token)
+        return response
+
+    else:    
         return render_template(
             'login.html'
         )
