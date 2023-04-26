04124e74e9cae6be4acf7970dd17a23c006b74b7
Caleb Styles <cstyles2@unca.edu>
2023-04-26 15:58:45 -0400

    Changed index file

--- homework/hw07/react-client/src/index.js
+++ homework/hw07/react-client/src/index.js
@@ -4,6 +4,8 @@ import App from './App';
 import './index.css';
 import {getAccessToken, getCookie} from './utils';
 
+const token = await getAccessToken('webdev', 'password');
+
 function renderApp(token) {
     ReactDOM.render(
         <App token={token} />,
@@ -11,20 +13,4 @@ function renderApp(token) {
     );
 }
 
-async function getAccessTokenAndRenderApp() {
-    // this initializes the app after the access token is set.
-    const csrf = getCookie('csrf_access_token') ;
-    if (csrf && window.location.port !== '3000' && window.location.port !== '5000') {
-        // this executes if the app is run within flask:
-        console.log('Authentication handled via CSRF + Http-only cookie.')
-        renderApp();
-    } else {
-        console.log(window.location.port === '3000')
-        // this executes if the app is run via npm start
-        console.log('Authentication handled via REST API Token.')
-        const token = await getAccessToken('webdev', 'password');
-        renderApp(token);
-    }
-}
-
-getAccessTokenAndRenderApp();
+renderApp();
